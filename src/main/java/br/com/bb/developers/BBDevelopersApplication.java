48c94df2e4e1cba065a41b52dbf57b5c305d1d61
fredyefra <fredweb3@gmail.com>
2022-12-22 21:02:35 -0300

    FIX: Mudança de pacote classe que carrega projeto.

---
+++ src/main/java/br/com/bb/developers/BBDevelopersApplication.java
@@ -0,0 +1,13 @@
+package br.com.bb.developers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BBDevelopersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BBDevelopersApplication.class, args);
+	}
+
+}
