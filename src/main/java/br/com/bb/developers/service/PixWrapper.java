18d117572823bce634186fca7bcf7652d6de05a0
fredyefra <fredweb3@gmail.com>
2022-12-25 20:58:51 -0300

    FEAT: Inclusão da classe PixWrapper.

---
+++ src/main/java/br/com/bb/developers/service/PixWrapper.java
@@ -0,0 +1,20 @@
+package br.com.bb.developers.service;
+
+import org.springframework.web.reactive.function.client.WebClient;
+
+import br.com.bb.developers.model.Pix;
+
+public interface PixWrapper {
+
+	/**
+	 * Disponibiliza o objeto Pix os parametros e credencias estão disponiveis no portal
+	 * developers bb.
+	 * @return Pix
+	 * @author proitec-legacy
+	 * @see Pix
+	 * @see WebClient
+	 * @param bearer - portal developers BB
+	 */
+	public Pix pixObject(String bearer);
+	
+}
