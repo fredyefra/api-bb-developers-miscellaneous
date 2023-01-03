85b69295aebca19ae62dd0966f72de2a91580f22
fredyefra <fredweb3@gmail.com>
2023-01-02 23:32:30 -0300

    FEAT: Inclusão classe EndPoint.

---
+++ src/main/java/br/com/bb/developers/util/endpoint/EndPoint.java
@@ -0,0 +1,13 @@
+package br.com.bb.developers.util.endpoint;
+
+
+/**
+ * @author proitec-legacy
+ * Classe com os endpoints disponíveis no bb developers.
+ */
+public class EndPoint {
+
+	public static final String ENDPOINT_TOKEN = "https://oauth.hm.bb.com.br/oauth/token";
+	
+	
+}
