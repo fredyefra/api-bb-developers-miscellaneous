4f76fd54b312da2b9b64bc50d59002fca0062f34
fredyefra <fredweb3@gmail.com>
2023-01-03 22:41:43 -0300

    IPROVEMENT: Classe swagger.

--- src/main/java/br/com/bb/developers/util/SwaggerConfig.java
+++ src/main/java/br/com/bb/developers/util/SwaggerConfig.java
@@ -2,11 +2,15 @@ package br.com.bb.developers.util;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,19 +23,28 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors
-				.basePackage("br.com.bb.developers.controller"))
+				//.apis(RequestHandlerSelectors.basePackage("br.com.bb.developers.controller"))
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
 				.paths(PathSelectors.any())
-				.build();
+				.build()
+				.apiInfo(info());
 	}
 
-	@Override
+	@Override 
 	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
 
 		registry.addResourceHandler("swagger-ui.html")
-		   .addResourceLocations("classpath:/META-INF/resources/");
+		.addResourceLocations("classpath:/META-INF/resources/");
 
 		registry.addResourceHandler("/webjars/**")
-		   .addResourceLocations("classpath:/META-INF/resources/webjars/");
-	}
+		.addResourceLocations("classpath:/META-INF/resources/webjars/"); }
+
+	private ApiInfo info() {
+        return new ApiInfoBuilder()
+                .title("api-bb-developers-miscellaneous.")
+                .description("miscellaneous api`s from portal bb developers.")
+                .version("1.0.0")
+                .contact(new Contact("fredyefra", "https://github.com/fredyefra", "fredyefra@xpto.com"))
+                .build();
+    }
 }
