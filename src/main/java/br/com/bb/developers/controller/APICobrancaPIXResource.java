2d731d451d501f0e6e5fbb8f9ba93d19b40d38a9
fredyefra <fredweb3@gmail.com>
2023-01-03 22:42:47 -0300

    IPROVEMENT: Classe Pix cobrança.

--- src/main/java/br/com/bb/developers/controller/APICobrancaPIXResource.java
+++ src/main/java/br/com/bb/developers/controller/APICobrancaPIXResource.java
@@ -3,7 +3,6 @@ package br.com.bb.developers.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,11 +14,14 @@ import br.com.bb.developers.model.Pix;
 import br.com.bb.developers.service.PixWrapper;
 import br.com.bb.developers.service.TokenWrapper;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Hidden;
 
-@CrossOrigin("http://localhost:8080") // Swagger Annotation
-@Api(value = "pix-resource") // Swagger Annotation
+//@CrossOrigin("*") 
+@Api(value = "pix-resource") 
 @RestController
 @RequestMapping(value = "developers/bb/cobranca-pix/v1")
+@Hidden
 public class APICobrancaPIXResource {
 
 	@Autowired
@@ -28,12 +30,14 @@ public class APICobrancaPIXResource {
 	@Autowired
 	private PixWrapper pix;
 	
+	@ApiOperation(value = "Token de acesso deve ser informado nas requisições da API'S.")
 	@PostMapping
 	public ResponseEntity<String> gerarToken(@RequestHeader(required = true, name = "Authorization") String basic) {
 		String bearer = token.tokenString("client_credentials", "cob.write cob.read pix.read pix.write", basic);
 		return ResponseEntity.ok().body(bearer);
 	}
 
+	@ApiOperation(value = "Gerar cobrança PIX.")
     @PutMapping(value= "/gerar-cobranca-pix")
 	public ResponseEntity<Pix> gerarCobranca(@RequestHeader(required = true, name = "Authorization") String bearer,
     @RequestBody Pix cobranca)
