75e47dd3b43c116ad17656dbb2d34474eb09fad9
fredyefra <fredweb3@gmail.com>
2023-01-05 20:52:28 -0300

    IMPROVEMENT: Refatoração classe Endponint.

---
+++ src/main/java/br/com/bb/developers/util/endpoints/EndPoint.java
@@ -0,0 +1,23 @@
+package br.com.bb.developers.util.endpoints;
+
+/**
+ * @author proitec-legacy Classe com os endpoints disponíveis no bb developers.
+ */
+public class EndPoint {
+
+	/**
+	 * Constante equivalente a "https://oauth.hm.bb.com.br/oauth/token".
+	 */
+	public static final String ENDPOINT_TOKEN = "https://oauth.hm.bb.com.br/oauth/token";
+
+	/**
+	 * Constante equivalente a "https://api.hm.bb.com.br/validacao-contas/v1".
+	 */
+	public static final String ENDPOINT_CONTA_ATIVA = "https://api.hm.bb.com.br/validacao-contas/v1";
+
+	/**
+	 * Constante equivalente a "https://api.hm.bb.com.br/pix/v1/cob/".
+	 */
+	public static final String ENDPOINT_PIX_COBRANCA = "https://api.hm.bb.com.br/pix/v1/cob/";
+
+}
