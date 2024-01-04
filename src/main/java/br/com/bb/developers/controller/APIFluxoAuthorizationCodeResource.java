f2b9a913319a277934d40961efffc53c62bea40c
fredyefra <fredweb3@gmail.com>
2024-01-03 22:24:49 -0300

    FEAT: Inclusão Fluxo Auth Code (Oauth).

---
+++ src/main/java/br/com/bb/developers/controller/APIFluxoAuthorizationCodeResource.java
@@ -0,0 +1,30 @@
+package br.com.bb.developers.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.bb.developers.service.FluxoAuthorizationCodeWrapper;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Parameter;
+import reactor.core.publisher.Mono;
+
+
+@Api(value = "fluxo-autorizacao-code", tags = "Endpoint para demonstrar o fluxo de autorização code.")
+@RestController
+@RequestMapping(value = "developers/bb/fluxo-auth-code/v1")
+public class APIFluxoAuthorizationCodeResource {
+
+	@Autowired
+	private FluxoAuthorizationCodeWrapper fluxoAuthCode;
+
+	//@RequestParam  (required = true, name = "gw-dev-app-key")  String gw_dev_app_key,
+	@GetMapping(value = "/fluxo-auth-code")
+	public ResponseEntity<Mono<String>> authCode(@RequestParam (required = true, name = "client_id")@Parameter(example = "client_id", description = "client_id") String client_id) {
+		
+		return ResponseEntity.ok(fluxoAuthCode.authorizationCodehString(client_id));
+	}
+}
