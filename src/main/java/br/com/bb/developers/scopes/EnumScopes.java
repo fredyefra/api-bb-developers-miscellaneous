d4aae7c4fa69f5d8301f8f079b7e3408eb2401b6
fredyefra <fredweb3@gmail.com>
2024-01-04 23:12:28 -0300

    IMPROVEMENT: Inclusão escopo fluxo auth code (Oauth).

--- src/main/java/br/com/bb/developers/scopes/EnumScopes.java
+++ src/main/java/br/com/bb/developers/scopes/EnumScopes.java
@@ -1,14 +1,15 @@
 package br.com.bb.developers.scopes;
 
 /**
- * @author proitec-legacy
+ * @author fredyefra
  * Enum's para representar os escopes de cada ENDPOINT.
  */
 public enum EnumScopes {
 
 	ESCOPO_CONTA_ATIVA("validacao-contas.info"), 
 	ESCOPO_COBRANCA_PIX("cob.write cob.read pix.read pix.write"),
-	ESCOPO_BB_PAY("checkout.solicitacao-pagamento-requisicao checkout.pagamentos-requisicao checkout.pagamentos-info checkout.marcas-ativas-info");
+	ESCOPO_BB_PAY("checkout.solicitacao-pagamento-requisicao checkout.pagamentos-requisicao checkout.pagamentos-info checkout.marcas-ativas-info"),
+	ESCOPO_FLUXO_AUTH_CODE("openid-otp");
 	
 	private String escopo;
 
